9ed258af73fb3c4503847635abe0383a0f084545
Allan Vialatte <allan.vialatte@icloud.com>
2018-04-20 18:35:29 +0200

    Fix Header

--- Tests/SKCoreTests/SKCoreTests.swift
+++ Tests/SKCoreTests/SKCoreTests.swift
@@ -1,3 +1,26 @@
+//
+// Action.swift
+//
+// Copyright © 2017 Peter Zignego. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 import XCTest
 @testable import SKCore
 
@@ -7,7 +30,7 @@ final class SKCoreTests: XCTestCase {
   func testEvents() {
     let data = Helper.JSONData.events
     let json:[String:Any] = try! JSONSerialization.jsonObject(with: data, options: []) as! [String : Any]
-    let events = self.eventsKeys.map { json[$0] as! [String: Any] }.map { Event($0) }
+    _ = self.eventsKeys.map { json[$0] as! [String: Any] }.map { Event($0) }
     
   }
   
